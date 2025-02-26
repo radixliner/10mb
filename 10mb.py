7894453a84c031bee2ff6bf756b0b823925db9c0
Radix <rifqigamer46@gmail.com>
2025-02-26 17:57:48 +0700

    Fuck off https://twitter.com/DiscordPreviews/status/1831066626411880866

--- 10mb.py
+++ 10mb.py
@@ -4,7 +4,6 @@ import subprocess
 import os
 
 def get_duration(fileInput):
-
     return float(
         subprocess.check_output([
             "ffprobe",
@@ -14,7 +13,6 @@ def get_duration(fileInput):
             fileInput
         ])[:-1]
     )
-
 
 def transcode(fileInput, fileOutput, bitrate):
     command = [
@@ -29,27 +27,23 @@ def transcode(fileInput, fileOutput, bitrate):
             'copy',
             fileOutput
       ]
-    #print(command)
     proc = subprocess.run(
                 command,
                 capture_output=True,
-                # avoid having to explicitly encode
                 text=True
     )
-    #print(proc.stdout)
 
-# Tolerance below 8mb
+# Tolerance below 10mb
 tolerance = 10
 fileInput = sys.argv[1]
 fileOutput = fileInput + ".crushed.mp4"
-targetSizeKilobytes = 8192
+targetSizeKilobytes = 10240  # 10 MB in kilobytes
 targetSizeBytes = targetSizeKilobytes * 1024
 durationSeconds = get_duration(sys.argv[1])
-bitrate = round( targetSizeBytes / durationSeconds)
+bitrate = round(targetSizeBytes / durationSeconds)
 beforeSizeBytes = os.stat(fileInput).st_size
 
 factor = 0
-
 attempt = 0
 while (factor > 1.0 + (tolerance/100)) or (factor < 1):
     attempt = attempt + 1
